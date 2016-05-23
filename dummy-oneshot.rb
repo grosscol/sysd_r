5798003b47312bacda45421ebd4f28f64fbed155
Colin <grosscol@gmail.com>
2016-05-23 16:07:34 -0400

    Add explanatory comments to one shot ruby service.

--- dummy-oneshot.rb
+++ dummy-oneshot.rb
@@ -1,3 +1,9 @@
 require_relative './obs-logger.rb'
 
+# One shot systemd units are meant to be fire and forget.
+# This is the one shot service file.  The corresponding systemd unit will invoke this file.
+
+# Log environment etc.
 ObsLogger.logall('obs-oneshot.txt')
+
+# Other testing code goes here.
